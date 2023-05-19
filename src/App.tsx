241b1749685be860658c9d1a55d37af12018f5ca
hodygmd <gmdhody@gmail.com>
2023-05-19 12:11:17 -0600

    fixes to deploy 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Outlet, Link} from "react-router-dom";
 import {
