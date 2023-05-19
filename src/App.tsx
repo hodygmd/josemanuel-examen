9211d9339184dd4230b999c13fde8eef2876cb08
hodygmd <gmdhody@gmail.com>
2023-05-19 16:32:42 -0600

    fixes to deploy 1.3

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import {
     NavLink,
 } from 'reactstrap';
 
-const About = React.lazy(() => import("./Pages/About"));
+const About = React.lazy(() => import("./About"));
 const Users = React.lazy(() => import("./Pages/Users"));
 const SubjectsCareers = React.lazy(() => import("./Pages/Subjects-Careers"));
 export default function App() {
